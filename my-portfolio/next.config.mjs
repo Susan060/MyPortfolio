7ea9ea8dab2d9280acab242fd7b2d8315e9a3155
Susan060 <sushantadhikari060@gmail.com>
2026-05-24 09:17:58 +0545

    This is nextjs updated

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,7 +1,36 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true, // disables ESLint errors blocking build
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//       },
+//     ],
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/Susan_Adhikari_CV_v3.pdf",
+//         destination: "/Susan_Adhikari_CV_v3.pdf",
+//       },
+//       {
+//         source: '/api/:path*',
+//         destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // disables ESLint errors blocking build
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
@@ -11,12 +40,25 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
+  async headers() {
     return [
       {
         source: "/Susan_Adhikari_CV_v3.pdf",
-        destination: "/Susan_Adhikari_CV_v3.pdf",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/pdf",
+          },
+          {
+            key: "Content-Disposition",
+            value: "inline; filename=Susan_Adhikari_CV_v3.pdf",
+          },
+        ],
       },
+    ];
+  },
+  async rewrites() {
+    return [
       {
         source: '/api/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
